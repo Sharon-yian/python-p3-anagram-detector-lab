fd8d81ff7836d23475a2f2d3775b70826bd656e6
Sharon Soyian Lenjir <sharon.lenjir@student.moringaschool.com>
2025-11-25 08:56:23 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        sorted_word = sorted(self.word)
+        return [candidate for candidate in candidates if sorted(candidate.lower()) == sorted_word and candidate.lower() != self.word]
+# Example usage:
+# anagram = Anagram("listen")
+# print(anagram.match(["enlist", "google", "inlets", "banana"]))  # Output: ['enlist', 'inlets']    
+
